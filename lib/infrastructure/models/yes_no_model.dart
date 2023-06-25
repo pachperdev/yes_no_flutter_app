1cd281509b3c7c7bf2c7b2f588f02c53a2dbddcd
Mauricio Pacheco <pachperdev@gmail.com>
2023-06-25 10:55:17 -0500

    API response mapped

---
+++ lib/infrastructure/models/yes_no_model.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+
+import 'package:yes_no_flutter_app/domain/entities/message.dart';
+
+class YesNoModel {
+  final String? answer;
+  final bool? forced;
+  final String? image;
+
+  YesNoModel({
+    this.answer,
+    this.forced,
+    this.image,
+  });
+
+  YesNoModel copyWith({
+    String? answer,
+    bool? forced,
+    String? image,
+  }) =>
+      YesNoModel(
+        answer: answer ?? this.answer,
+        forced: forced ?? this.forced,
+        image: image ?? this.image,
+      );
+
+  factory YesNoModel.fromRawJson(String str) =>
+      YesNoModel.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory YesNoModel.fromJson(Map<String, dynamic> json) => YesNoModel(
+        answer: json["answer"],
+        forced: json["forced"],
+        image: json["image"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "answer": answer,
+        "forced": forced,
+        "image": image,
+      };
+
+  Message toMessageEntity() => Message(
+        text: answer == 'yes' ? 'Si' : 'NO',
+        imageUrl: image,
+        fromWho: FromWho.her,
+      );
+}
--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_flutter_app/domain/helpers/get_yes_no_answer.dart';
 
 import '../../domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
   final scrollController = ScrollController();
   final List<Message> messagesList = [];
+  final GetYesNoAswer getYesNoAswer = GetYesNoAswer();
 
   Future<void> sendMessage(String text) async {
     if (text.isEmpty) return;
@@ -12,6 +14,17 @@ class ChatProvider extends ChangeNotifier {
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messagesList.add(newMessage);
 
+    if (text.endsWith('?')) {
+      herReplay();
+    }
+
+    notifyListeners();
+    scrollToBottom();
+  }
+
+  Future<void> herReplay() async {
+    final herMessage = await getYesNoAswer.getAnswer();
+    messagesList.add(herMessage);
     notifyListeners();
     scrollToBottom();
   }
